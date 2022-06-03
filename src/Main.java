f4cc84fa9b8076206a5eb02051976e211ef30fa4
Mal'eau <malo.herembert@eleve.isep.fr>
2022-06-03 18:45:55 +0200

    test commit repo

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		WDigraph<String> myWDiGraph = GraphFactory.createWDiGraphFromTextFile("graph-WDG.txt");
-		System.out.println(myWDiGraph.adjacencyList);
-		Dijkstra<String> myDijkstraSP = new Dijkstra<>(myWDiGraph, 0);
-		System.out.println("The graph contains no negative weight edge ? " + myDijkstraSP.verifyNonNegative(myWDiGraph));
+//		WDigraph<String> myWDiGraph = GraphFactory.createWDiGraphFromTextFile("graph-WDG.txt");
+//		System.out.println(myWDiGraph.adjacencyList);
+//		Dijkstra<String> myDijkstraSP = new Dijkstra<>(myWDiGraph, 0);
+//		System.out.println("The graph contains no negative weight edge ? " + myDijkstraSP.verifyNonNegative(myWDiGraph));
+
+		System.out.println("Test push repo git");
 	}
 }
