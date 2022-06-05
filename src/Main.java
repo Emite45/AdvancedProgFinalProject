1708ece7d1f7aa9c1a96eab2c8abcca072756b9d
Mal'eau <malo.herembert@eleve.isep.fr>
2022-06-05 23:23:03 +0200

    Fin du main

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,10 @@ public class Main
 
 		System.out.println("\nTest Dijkstra with graph-WDG.txt");
 		Dijkstra<String> dijkstraMetro = new Dijkstra<>();
-		long start = System.currentTimeMillis();
+		long start1 = System.currentTimeMillis();
 		dijkstraMetro.dijkstraSP(graphMetro, "80", "105");
-		long end = System.currentTimeMillis();
-		System.out.println("Execution time: " + (end - start) + " ms");
+		long end1 = System.currentTimeMillis();
+		System.out.println("Execution time: " + (end1 - start1) + " ms");
 
 
 
@@ -40,33 +40,19 @@ public class Main
 		long end2 = System.currentTimeMillis();
 		System.out.println("Execution time: " + (end2 - start2) + " ms");
 
-//		Dijkstra<String> myDijkstraSP = new Dijkstra<String>();
-//		myDijkstraSP.dijkstraSP(myWDiGraph, "4", "3");
-//		BellmanFord<String> myBellmanFord = new BellmanFord<String>();
-//		myBellmanFord.BellmanFordSP(myWDiGraph, "4", "3");
-//		System.out.println("The graph contains no negative weight edge ? " + myDijkstraSP.verifyNonNegative(myWDiGraph));
-//		Map<Integer, String> stationListMap = new HashMap<>();
-//		WDigraph<String> myGraph = GraphFactory.createWDiGraphFromMetroFile("metro.txt", stationListMap);
-//		System.out.println("number of nodes : " + myGraph.adjacencyList.size());
-//		int numberOfEdges = 0;
-//		for (Map.Entry<String, ArrayList<DirectedEdge<String>>> mapElement : myGraph.adjacencyList.entrySet()) {
-//			numberOfEdges += mapElement.getValue().size();
-//			
-//		}
-//		System.out.println("number of edges : " + numberOfEdges);
-//		System.out.println("Test push repo git");
-
-//		DijkstraBidirectional<String> myDijkstraBidirectional = new DijkstraBidirectional<>();
-//		myDijkstraBidirectional.dijkstraBidirectionalSP(myWDiGraph, "0", "7");
 
 
+		//Test BellmanFord
+		System.out.println("");
+		BellmanFord<String> bellmanFord = new BellmanFord<String>();
+		bellmanFord.BellmanFordSP(graphWDG, "0", "7");
 
+		System.out.println("");
 
-
-
-//
-//		DijkstraBidirectional<String> myBiDijkstraSP = new DijkstraBidirectional<>();
-//		myBiDijkstraSP.dijkstraBidirectionalSP(myGraph, "80", "105");
-
+		BellmanFord<String> bellmanFordMetro = new BellmanFord<String>();
+		long start3 = System.currentTimeMillis();
+		bellmanFordMetro.BellmanFordSP(graphMetro, "80", "105");
+		long end3 = System.currentTimeMillis();
+		System.out.println("Execution time: " + (end3 - start3) + " ms");
 	}
 }
