a6a615e4f1aff91fa37f84d3d47e469d39c27e5e
Mal'eau <malo.herembert@eleve.isep.fr>
2022-06-05 20:27:48 +0200

    Dijkstra Bidirectionel et modif dijkstra

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,29 @@
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		WDigraph<String> myWDiGraph = GraphFactory.createWDiGraphFromTextFile("graph-WDG-n.txt");
-		System.out.println(myWDiGraph);
-
-		DijkstraSP<String> myDijkstraSP = new DijkstraSP<>(myWDiGraph, "0", "7");
-//		System.out.println("The graph contains no negative weight edge ? " + myDijkstraSP.verifyNonNegative(myWDiGraph));
-//		Map<Integer, String> stationListMap = new HashMap<>();
-//		WDigraph<String> myGraph = GraphFactory.createWDiGraphFromMetroFile("metro.txt", stationListMap);
-//		System.out.println("number of nodes : " + myGraph.adjacencyList.size());
-//		int numberOfEdges = 0;
-//		for (Map.Entry<String, ArrayList<DirectedEdge<String>>> mapElement : myGraph.adjacencyList.entrySet()) {
-//			numberOfEdges += mapElement.getValue().size();
-//			
-//		}
-//		System.out.println("number of edges : " + numberOfEdges);
-//		System.out.println("Test push repo git");
-
-
-
-//		Map<String, Integer> map = new HashMap<>()
-//		{{
-//			put("0", 0);
-//			put("1", 1);
-//			put("2", 2);
-//			put("3", 3);
-//			put("4", 4);
-//		}};
+//		WDigraph<String> myWDiGraph = GraphFactory.createWDiGraphFromTextFile("graph-WDG.txt");
+//		System.out.println(myWDiGraph);
 //
-//		List<Integer> values = new ArrayList<>(map.values());
-//		System.out.println(Collections.min(values));
-//		System.out.println(Collections.max(values));
+//		Dijkstra<String> myDijkstra = new Dijkstra<>();
+//		myDijkstra.dijkstraSP(myWDiGraph, "A", "G");
 
 
-//		List<Integer> list = new ArrayList<>()
-//		{{
-//			add(1);
-//			add(2);
-//			add(3);
-//		}};
-//
-//		List<Integer> list2 = new ArrayList<>();
-//		list2.addAll(list);
-//		list2.add(4);
-//
-//		System.out.println(list);
-//		System.out.println(list2);
+//		DijkstraBidirectional<String> myDijkstraBidirectional = new DijkstraBidirectional<>();
+//		myDijkstraBidirectional.dijkstraBidirectionalSP(myWDiGraph, "0", "7");
+
+
+
+
+
+		Map<Integer, String> stationListMap = new HashMap<>();
+		WDigraph<String> myGraph = GraphFactory.createWDiGraphFromMetroFile("metro.txt", stationListMap);
+
+		DijkstraBidirectional<String> myDijkstraSP = new DijkstraBidirectional<>();
+		myDijkstraSP.dijkstraBidirectionalSP(myGraph, "80", "105");
+
 	}
 }
